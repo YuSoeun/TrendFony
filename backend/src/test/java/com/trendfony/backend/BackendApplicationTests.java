d66a5c61942a7132022d27d0b0450f653b9984a7
YuSoeun <doplinblue@handong.ac.kr>
2024-11-28 19:28:45 +0900

    feat[#4]: Detail table rank 관련 오류 수정

--- backend/src/test/java/com/trendfony/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/trendfony/backend/BackendApplicationTests.java
@@ -8,9 +8,9 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test") // 테스트 환경 활성화
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-		// 애플리케이션 컨텍스트 로딩 테스트
-		System.out.println("Test context loads successfully!");
-	}
+//	@Test
+//	void contextLoads() {
+//		// 애플리케이션 컨텍스트 로딩 테스트
+//		System.out.println("Test context loads successfully!");
+//	}
 }
